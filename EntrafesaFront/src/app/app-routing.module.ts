b7d8e489e7841699f6daec3ce20d188b9aa1ac64
Rafaelito2002 <rafaelibezu@gmail.com>
2024-10-04 23:50:54 -0500

    Avance Navbar, banner, itinerario

--- EntrafesaFront/src/app/app-routing.module.ts
+++ EntrafesaFront/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './componentes/nav/nav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'nav',
+    component: NavComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- EntrafesaFront/src/app/app.module.ts
+++ EntrafesaFront/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './componentes/nav/nav.component';
+import { MenubarComponent } from './componentes/menubar/menubar.component';
+import { ItinerarioComponent } from './componentes/itinerario/itinerario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MenubarComponent,
+    ItinerarioComponent
   ],
   imports: [
     BrowserModule,
